35019090b3b5a8f7cf84be735f0874157ce4e523
wultes <workwultes@gmail.com>
2020-04-16 22:33:19 +0300

    new version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='vkbotsapi',
     description='VK API module for Python',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/wultes/vkbotsapi',
     license='MIT',
     author='Wultes (Kirill)',
